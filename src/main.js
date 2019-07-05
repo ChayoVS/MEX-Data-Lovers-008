aad654dda8db6eccb4dffc6c803673fa75fd7cbd
rosario VS <paradisekiss-yaoi@hotmail.com>
2019-07-05 11:57:39 -0500

    Cambios html, ya se ven los avatares, css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-// let = LOL;
-// for (let i = 0; i<LOL.length; i++){
-//     console.log (LOL[i].Aatrox)
-// }
+const printAllAvatar = document.getElementById("allAvatar"); 
+const dataBase = LOL;
+let champions = Object.values(dataBase.data);
+
+//En vez de usar forEach, uso for
+
+const showChampions= (champions) => {
+  let str = "";
+  for (let i = 0; i < champions.length; i++) {
+    str += `<div  class="img-champions" >
+        <img id="img-avatar" src="${champions[i].img}">
+        </img>
+        <br>
+      <div>
+       ${champions[i].name}
+      </div>
+    </div>`;
+  }
+
+  printAllAvatar.innerHTML = str;
+};
+
+showChampions(champions);
