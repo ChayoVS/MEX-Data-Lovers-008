0d3f1be2c53a222704a6369adae75b9292510ea4
rosario VS <paradisekiss-yaoi@hotmail.com>
2019-08-03 09:29:28 -0500

    Prueba nueva dataLovers

---
+++ src/data.js
@@ -0,0 +1,45 @@
+
+//filtrar por rol del campeón 
+const roleBtn = document.getElementsByClassName('roleBtn');
+// console.log(roleBtn);
+for (let index = 0; index < roleBtn.length; index++) {
+  roleBtn[index].addEventListener ('click', ()=> {
+    //   document.getElementById('allAvatar').innerHTML= "Hola";
+    allAvatar.style.display = 'none'
+    const idTarget = event.target.id;
+    const roleClick = newArray.filter(championRole => championRole.tags[0] == idTarget || championRole.tags[1] == idTarget);
+    // console.log(roleClick);
+    printByRole(roleClick)
+  })
+}
+
+
+
+
+
+
+
+
+
+
+// const sorData= (newArray, sortBy, sortOrder) => {
+//     let newOrderData = [];
+//     switch (sortOrder) {
+//         case 'ascendant':
+//             if(sortBy === 'id') {
+//                 newOrderData = newArray.sort ((a,b)=> (a.id > b.id ? 1:-1));
+//             } 
+//             break;
+  
+//         case 'descendant':
+//             if(sortBy === 'id') {
+//                 newOrderData = newArray.sort ((a,b) => (a.id > b.id ? 1:-1));
+//             }
+//             break;
+//     }
+//     return newOrderData;
+//   };
+
+
+// window.roleBtn = roleBtn
+// window.sorData = sorData
