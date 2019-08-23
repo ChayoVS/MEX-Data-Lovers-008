98dba70fce4fe7a28290aa7906b83c9e21071796
rosario VS <paradisekiss-yaoi@hotmail.com>
2019-08-23 11:59:54 -0500

    Agregando CSS

--- src/main.js
+++ src/main.js
@@ -32,35 +32,35 @@ const dataBase = LOL.data;
 const newArray = Object.values(dataBase);
 //accediendo a Aatrox en el nuevo arreglo 
 //let newDataChamp = newArray[0];
-const showCard = document.getElementById('cards-champion');
+// const showCard = document.getElementById('cards-champion');
 
-//usando la data como array para acceder a datos específicos
-let dataForIndividualCard = '';
-newArray.forEach(element => {
-  const imagenChamp = element.splash
-  const nameChamp = element.id;
-  const titleChamp = element.title
-  const roleChamp = element.tags;
-  const descriptionChamp = element.blurb;
-  //Se muestra en consola lo siguiente:
-  // console.log(nameChamp);
-  // console.log(roleChamp)
-  // console.log(description)
+// //usando la data como array para acceder a datos específicos
+// let dataForIndividualCard = '';
+// newArray.forEach(element => {
+//   const imagenChamp = element.splash
+//   const nameChamp = element.id;
+//   const titleChamp = element.title
+//   const roleChamp = element.tags;
+//   const descriptionChamp = element.blurb;
+//   //Se muestra en consola lo siguiente:
+//   // console.log(nameChamp);
+//   // console.log(roleChamp)
+//   // console.log(description)
 
-  //Pintar en html
-  dataForIndividualCard += `<article id="cards" class="col-8">
-  <img class="iconX" src="../img/delete-button.png">
-  <img id="absoluteImg" src= "${imagenChamp}" alt="ImagenChampion">
-  <div id= "descChamp">
-  <h2>${nameChamp}</h2> <br>
-  <p>${titleChamp}</p> <br>
-  <p>${roleChamp} </p> <br>
-  <p>${descriptionChamp} </p>
-  </div>
-  </article>`;
-});
+//   //Pintar en html
+//   dataForIndividualCard += `<article id="cards" class="col-8">
+//   <img class="iconX" src="../img/delete-button.png">
+//   <img id="absoluteImg" src= "${imagenChamp}" alt="ImagenChampion">
+//   <div id= "descChamp">
+//   <h2>${nameChamp}</h2> <br>
+//   <p>${titleChamp}</p> <br>
+//   <p>${roleChamp} </p> <br>
+//   <p>${descriptionChamp} </p>
+//   </div>
+//   </article>`;
+// });
 
-showCard.innerHTML= dataForIndividualCard;
+// showCard.innerHTML= dataForIndividualCard;
 
 
 
@@ -102,48 +102,48 @@ showChampions(newArray)
 // const onlyAssassins = newArray.filter(championRole => championRole.tags[0] =="Assassin");
 // console.log(onlyAssassins);
 
-const newData= [];
-const rolePage = document.getElementById('rolePage');
+// const newData= [];
+// const rolePage = document.getElementById('rolePage');
 
-const printByRole = (newArray) => {
-  let stringV ='';
-  newArray.forEach(element =>{
-    stringV += `<div class="flip-card" id="flipCard">
-    <div class="flip-card-inner">
-        <div id="front-card">
-            <img id="bigImg" src="${element.splash}" alt="imageChampion">
-            <div class="champInfo">
-                <h3>${element.id}</h3>
-            </div>
-        </div>
-        <div id="back-card">
-        <div class="champStats">
-            <br>
-            <h3>${element.id}</h3>
-            <h4>Information</h4> <br>
-            <p>Attack: ${element.info.attack}</p>
-            <p>Defense: ${element.info.defense}</p>
-            <p>Magic: ${element.info.magic}</p>
-            <p>Difficulty: ${element.info.difficulty}</p> <br>
-            <h4>Stats</h4> <br>
-            <p>HP: ${element.stats.hp}</p>
-            <p>MP: ${element.stats.mp}</p>
-            <p>Move speed: ${element.stats.movespeed}</p>
-            <p>Armor: ${element.stats.armor}</p>
-            <p>Spell block: ${element.stats.spellblock}</p>
-            <p>Attack range: ${element.stats.attackrange}</p>
-            <p>HP regeneration: ${element.stats.hpregen}</p>
-            <p>MP regeneration: ${element.stats.mpregen}</p>
-            <p>Crit: ${element.stats.crit}</p>
-            <p>Attack damage: ${element.stats.attackdamage}</p>
-            <p>Attack speed: ${element.stats.attackspeedoffset}</p> <br><br>
-        </div>
-    </div>
-    </div>
-</div>`
-  })
-  rolePage.innerHTML = stringV;
-}
+// const printByRole = (newArray) => {
+//   let stringV ='';
+//   newArray.forEach(element =>{
+//     stringV += `<div class="flip-card" id="flipCard">
+//     <div class="flip-card-inner">
+//         <div id="front-card">
+//             <img id="bigImg" src="${element.splash}" alt="imageChampion">
+//             <div class="champInfo">
+//                 <h3>${element.id}</h3>
+//             </div>
+//         </div>
+//         <div id="back-card">
+//         <div class="champStats">
+//             <br>
+//             <h3>${element.id}</h3>
+//             <h4>Information</h4> <br>
+//             <p>Attack: ${element.info.attack}</p>
+//             <p>Defense: ${element.info.defense}</p>
+//             <p>Magic: ${element.info.magic}</p>
+//             <p>Difficulty: ${element.info.difficulty}</p> <br>
+//             <h4>Stats</h4> <br>
+//             <p>HP: ${element.stats.hp}</p>
+//             <p>MP: ${element.stats.mp}</p>
+//             <p>Move speed: ${element.stats.movespeed}</p>
+//             <p>Armor: ${element.stats.armor}</p>
+//             <p>Spell block: ${element.stats.spellblock}</p>
+//             <p>Attack range: ${element.stats.attackrange}</p>
+//             <p>HP regeneration: ${element.stats.hpregen}</p>
+//             <p>MP regeneration: ${element.stats.mpregen}</p>
+//             <p>Crit: ${element.stats.crit}</p>
+//             <p>Attack damage: ${element.stats.attackdamage}</p>
+//             <p>Attack speed: ${element.stats.attackspeedoffset}</p> <br><br>
+//         </div>
+//     </div>
+//     </div>
+// </div>`
+//   })
+//   rolePage.innerHTML = stringV;
+// }
 
 const orderABC = document.getElementById('ascendant');
 const orderZYX = document.getElementById('descendant')
@@ -165,7 +165,7 @@ const searchInput = document.getElementById('buscar');
 const functionToSearch = () => {
   let getInputValue = searchInput.value;
   let firstCharacterToUpperCase = getInputValue.substring(0, 1).toUpperCase() + getInputValue.substring(1).toLowerCase();
-  let searchName = window.filterData(newArray, 'name', firstCharacterToUpperCase);
+  let searchName = filterData(newArray, 'name', firstCharacterToUpperCase);
   showChampions(searchName);
 };
 searchInput.addEventListener('keyup', functionToSearch);
